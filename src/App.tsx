9c4fe224893429af22cf47e372acda446a9c54d0
abuukarhaaji <abuukarhaaji9@gmail.com>
2025-07-12 00:12:24 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+        <Analytics/>
       </div>
     );
   }
