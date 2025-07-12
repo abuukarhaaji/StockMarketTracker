8854dd3a80c7b8eb4f8c550a2a2234ac93828a5d
abuukarhaaji <abuukarhaaji9@gmail.com>
2025-07-12 07:19:47 +0300

    Updated AnalyticsCards.tsx

--- src/components/Dashboard/AnalyticsCards.tsx
+++ src/components/Dashboard/AnalyticsCards.tsx
@@ -17,7 +17,7 @@ const formatCurrency = (amount: number) => {
     style: 'currency',
     currency: 'GBP',
     minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    maximumFractionDigits: 2,
   }).format(amount);
 };
 
